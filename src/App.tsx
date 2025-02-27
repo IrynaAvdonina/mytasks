ba9a54d63e5546b57198bfbc6a2d21f727c6dd09
Iryna <avdoninairin@gmail.com>
2025-02-27 15:18:00 +0200

    added font and mui icons, delete css files and sketch structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import './App.css'
+import { Box, Button, Container, CssBaseline, ThemeProvider, Typography } from '@mui/material';
+import { theme } from './theme/index.ts';
+import TaskList from './components/TaskList/TaskList';
+import SearchFilter from './components/SearchFilter/SearchFilter.tsx';
+import { Header } from './components/Header/Header.tsx';
 
 function App()
 {
 
   return (
-    <>
-
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <Header />
+        <Box component="main">
+          <Box component="section">
+            <SearchFilter />
+            <Box className="tasks-header">
+              <Typography component="span">2 tasks left</Typography>
+              <Button variant="text">Clear all tasks</Button>
+            </Box>
+            <TaskList />
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   )
 }
 
-export default App
+export default App;
