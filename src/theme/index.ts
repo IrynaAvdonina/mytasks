394070cea91df926af4e9232c497e20c1fac4e0c
Iryna <avdoninairin@gmail.com>
2025-03-05 13:07:39 +0200

    create palette

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,7 +1,34 @@
-import { createTheme } from '@mui/material';
+import { alpha, createTheme } from '@mui/material';
+declare module '@mui/material/styles' {
+  interface Palette
+  {
+    lightGrey: Palette['primary'];
+  }
+
+  interface PaletteOptions
+  {
+    lightGrey?: PaletteOptions['primary'];
+  }
+}
+
+// Update the Button's color options to include an ochre option
+declare module '@mui/material/Button' {
+  interface ButtonPropsColorOverrides
+  {
+    lightGrey: true;
+  }
+}
+const lightGrey = '#777676';
 
 export const theme = createTheme({
   typography: {
     fontFamily: 'Poppins, sans-serif',
+  },
+  palette: {
+    lightGrey: {
+      main: lightGrey,
+      light: alpha(lightGrey, 0.5),
+      dark: alpha(lightGrey, 0.9)
+    },
   },
 });
