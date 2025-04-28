5857c39d6f41aac60e79f8a393c895106ad5b8b9
IrynaAvdonina <alloallo714@gmail.com>
2025-04-28 16:57:06 +0300

    added new theme color

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,45 +1,46 @@
 import { alpha, createTheme, getContrastRatio } from '@mui/material';
 
 declare module '@mui/material/styles' {
-  interface Palette
-  {
-    lightGrey: Palette['primary'];
+  interface Palette {
+    silver: Palette['primary'];
     purple: Palette['primary'];
+    lightGrey: string;
   }
 
-  interface PaletteOptions
-  {
-    lightGrey?: PaletteOptions['primary'];
+  interface PaletteOptions {
+    silver?: PaletteOptions['primary'];
     purple?: PaletteOptions['primary'];
+    lightGrey?: string;
   }
 }
 
 declare module '@mui/material/Button' {
-  interface ButtonPropsColorOverrides
-  {
-    lightGrey: true;
+  interface ButtonPropsColorOverrides {
+    silver: true;
     purple: true;
   }
 }
-const lightGrey = '#777676';
+const silver = '#777676';
 const purple = '#594D9D';
+const lightGrey = '#D6D6D6';
 
 export const theme = createTheme({
   typography: {
     fontFamily: 'Poppins, sans-serif',
   },
   palette: {
-    lightGrey: {
-      main: lightGrey,
-      light: alpha(lightGrey, 0.5),
-      dark: alpha(lightGrey, 0.9),
-      contrastText: getContrastRatio(lightGrey, '#fff') > 4.5 ? '#fff' : '#111',
+    silver: {
+      main: silver,
+      light: alpha(silver, 0.5),
+      dark: alpha(silver, 0.9),
+      contrastText: getContrastRatio(silver, '#fff') > 4.5 ? '#fff' : '#111',
     },
     purple: {
       main: purple,
       light: alpha(purple, 0.5),
       dark: alpha(purple, 0.9),
       contrastText: getContrastRatio(purple, '#fff') > 4.5 ? '#fff' : '#111',
-    }
+    },
+    lightGrey: lightGrey,
   },
 });
